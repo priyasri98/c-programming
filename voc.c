a6b17d0612840e31d6ab35449cece62f3ca1d0eb
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2017-09-15 05:09:20 +0530

    Create new file

---
+++ voc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char n;
+printf("\n Enter the character");
+scanf("%c",&n);
+if(n==A||n==E||n==I||n==o||n==u||n==a||n==e||n==i||n==o||n==u)
+{
+printf("\n the given character is vowel");
+}
+else
+{
+printf("\n The given character is  consonant");
+}
+return 0;
+}
