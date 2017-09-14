8dff7c62b351845cf545cd08e5b14103335ad060
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2017-09-15 05:28:47 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("\n Enter the symbol");
+scanf("%c",&c);
+if(ch>=65&&ch<=90)||(ch>=97&&ch<=122)
+{
+printf("\n Enter character is alphabet");
+}
+else
+{
+printf("\nEnter character is not an alphabet");
+}
+return 0;
+}
