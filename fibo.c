4e7d7c0279402d0860829da882c6e103faeddd11
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2017-09-29 16:15:15 +0530

    Update fibo.c

--- fibo.c
+++ fibo.c
@@ -3,13 +3,13 @@ int main()
 {
 int i,t=0,t1=1,n,sum;
 printf("\n Enter the number how much u want in the series");
-scanf("%d",n);
+scanf("%d",&n);
 printf("\n The series are");
 printf("\n%d\n%d",t,t1);
 for(i=1;i<=n-2;i++)
 {
 sum=t+t1;
-printf("%d",sum);
+printf("\n%d",sum);
 t=t1;
 t1=sum;
 }
