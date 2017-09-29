9927672c46e0645740a3bedd7af2c1b3568c2f19
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2017-09-29 16:12:25 +0530

    Create fibo.c

---
+++ fibo.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int i,t=0,t1=1,n,sum;
+printf("\n Enter the number how much u want in the series");
+scanf("%d",n);
+printf("\n The series are");
+printf("\n%d\n%d",t,t1);
+for(i=1;i<=n-2;i++)
+{
+sum=t+t1;
+printf("%d",sum);
+t=t1;
+t1=sum;
+}
+return 0;
+}
+
+
+
+
