d8ef626247550192ca1d4a049eaeb894db9e2416
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2018-01-10 16:46:52 +0530

    Create primetwo.c

---
+++ primetwo.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int i,s,e,p;
+printf("\nEnter the two intervals");
+scanf("%d%d",&s,&e);
+s=s+1;
+while(s<e)
+{
+p=0;
+for(i=2;i<s/2;++i)
+{
+if(s%i==0)
+{
+p=1;
+break;
+}
+}
+if(p==0)
+printf("%d",s);
+++s;
+return 0;
+}
