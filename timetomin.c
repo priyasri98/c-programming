82e67e897d5ae32e3a938dbac7081672b3e38aa4
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2018-01-21 11:41:41 +0530

    Update timetomin.c

--- timetomin.c
+++ timetomin.c
@@ -6,9 +6,9 @@ int min;
 printf("\nEnter the time in minutes");
 scanf("%d",&min);
 if((min/60)==1)
-printf("%\n%dd hour",min/60);
+printf("%\n%d hour",min/60);
 else
-printf("\n%dd hours",min/60);
+printf("\n%d hours",min/60);
 if((min%60)==1)
 printf("\n%d minutes\n",min%60);
 else
