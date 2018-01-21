0e1e06689e0ada35f073c239d37a25dcf7ce7a2b
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2018-01-21 11:40:22 +0530

    Create timetomin.c

---
+++ timetomin.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int min;
+printf("\nEnter the time in minutes");
+scanf("%d",&min);
+if((min/60)==1)
+printf("%\n%dd hour",min/60);
+else
+printf("\n%dd hours",min/60);
+if((min%60)==1)
+printf("\n%d minutes\n",min%60);
+else
+printf("\n%d min\n",min%60);
+return 0;
+}
