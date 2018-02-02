fb28cbc5fae3ad841ab84d150ccf869e121b912f
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2018-02-02 16:45:00 +0530

    Create elementwithindex.c

---
+++ elementwithindex.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a[n],i,n;
+printf("\n Enter the number of elements");
+scanf("%d",&n);
+printf("\n Enter the %d elements",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+printf("\n %d  %d",a[i],i);
+}
+return 0;
+}
