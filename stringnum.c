46ea98f9eaff49df7fbe3502a54c2df0193962d4
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2018-02-16 21:34:46 +0530

    Update stringnum.c

--- stringnum.c
+++ stringnum.c
@@ -2,18 +2,27 @@
 int main()
 {
 char a[20];
-int i,f=0;
+int i,f=0,n=0;
 printf("\n Enter the string");
 scanf("%s",a);
 for(i=0;a[i]!='\0';i++)
 {
+  n=n+1;
 if(a[i]=='0'||a[i]=='1'||a[i]=='2'||a[i]=='3'||a[i]=='4'||a[i]=='5'||a[i]=='6'||a[i]=='7'||a[i]=='8'||a[i]=='9')
 {
 f=f+1;
 }
 if(f==0)
+{
 printf("\nNo");
-else
+}
+else if(f==n)
+{
 printf("\nYes");
+}
+  else
+  {
+    printf("\nNo");
+  }
 return 0;
 }
