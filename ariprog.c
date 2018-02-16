279fea70944d0aaa1d4e2fccb9d420259932b437
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2018-02-16 22:12:02 +0530

    Update ariprog.c

--- ariprog.c
+++ ariprog.c
@@ -5,7 +5,7 @@ int i,n,a,d,sum=0;
 printf("\n Enter the number of arithmetic progression");
 scanf("%d",&n);
 printf("\n Enter a and d values");
-scanf("\%d%d",&a,&d);
+scanf("%d%d",&a,&d);
 for(i=1;i<=n;i++)
 {
 sum=sum+(a+(i*d));
