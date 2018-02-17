a61a388b608f3f51191dc7a6dffcb1e9e04b5ded
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2018-02-17 19:52:38 +0530

    Create nosplcha.c

---
+++ nosplcha.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[50];
+int i,c=0;
+printf("\nEnter the sentence");
+gets(a);
+for(i=0;a[i]!='\0';i++)
+{
+if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z'))
+{
+break;
+}
+else
+{
+c++;
+}
+}
+printf("\n%d",c);
+return 0;
+}
