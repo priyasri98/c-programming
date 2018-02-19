7f92a3d788f29c99fac449e4dbd5a7b6368b4eb5
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2018-02-19 15:52:28 +0530

    Create wostcmp.c

---
+++ wostcmp.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[50],b[50];
+int i,c=0,t=0;
+printf("\nEnter the first string");
+gets(a);
+printf("\nEnter the second string");
+gets(b);
+for(i=0;a[i]!=0;i++)
+{
+c++;
+}
+for(i=0;b[i]!=0;i++)
+{
+t++;
+}
+if(c>t)
+{
+printf("\n %s",a);
+}
+else if(t>c)
+{
+printf("\n %s",b);
+}
+else if(t==c)
+{
+printf("\n %s",b);
+}
+return 0;
+}
