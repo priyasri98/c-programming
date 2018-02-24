a839ba43d02086ca7be4c5f63211fdf27eef5cc2
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2018-02-24 21:54:32 +0530

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,N,t1=0,t2=1,nt;
+printf("\n Enter number of terms u want in series:");
+scanf("%d",&N);
+printf("\nFibonacci series\n");
+for(i=1;i<=N;++i)
+{
+printf("%d\n",t1);
+nt=t1+t2;
+t1=t2;
+t2=nt;
+}
+return 0;
+}
