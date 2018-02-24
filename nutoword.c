4efb568e1835a2af4f702ff94f9d833954ed88c8
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2018-02-24 20:49:45 +0530

    Create nutoword.c

---
+++ nutoword.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\nEnter any number to point in words");
+scanf("%d",&n);
+switch(n)
+{
+case 1:printf("\nOne");
+break;
+case 2:printf("\nTwo");
+break;
+case 3:printf("\nThree");
+break;
+case 4:printf("\nFour");
+break;
+case 5:printf("\nFive");
+break;
+case 6:printf("\n six");
+break;
+case 7:printf("\n Seven ");
+break;
+case 8:printf("\nEight");
+break;
+case 9:printf("\nNine");
+break;
+case 10:printf("\nTen");
+break;
+}
+return 0;
+}
