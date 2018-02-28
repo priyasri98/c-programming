3ce75b841d5f3338431e9d08bd0b8d7685da14ce
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2018-02-28 10:40:41 +0000

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+scanf("%d%d",&a,&b);
+if(a>b)
+{
+a=a-b;
+}
+else
+{
+a=b-a;
+}
+if((a%2==0)||(a%2==0))
+{
+printf("\nEven");
+}
+else
+{
+printf("\nodd");
+}
+return 0;
+}
