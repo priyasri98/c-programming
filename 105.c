e8446e65266291344fe330d4fcf2c137b4fae44b
priyasri98 <31505433+priyasri98@users.noreply.github.com>
2018-03-19 16:53:47 +0530

    Create 105.c

---
+++ 105.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int n;
+char a;
+scanf("%d",&n);
+a=64+n;
+printf("%c",a);
+return 0;
+}
+                                                                                                                                                  
